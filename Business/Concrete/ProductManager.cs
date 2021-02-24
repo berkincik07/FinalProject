44793282db7046f4c219fe0c64a4e3770aad6d4d
Root <Root@DESKTOP-89ILC7V>
2021-02-25 00:41:17 +0300

    Business Rules and Business Engine added.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,8 +1,10 @@
 ﻿using Business.Abstract;
+using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
@@ -11,6 +13,7 @@ using Entities.DTOs;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concrete
@@ -18,25 +21,35 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
-
-        public ProductManager(IProductDal productDal)
+        ICategoryService _categoryService;
+    
+        public ProductManager(IProductDal productDal,ICategoryService categoryService)
         {
             _productDal = productDal;
-        }
+            _categoryService = categoryService;
+         }
 
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
 
-            
-            //Loglama
-            //cacheremove
-            //authorezation
-            //Business Codes
+            IResult result = BusinessRules.Run(
+                              CheckIfProductCountOfCategoryCorrect(product.CategoryId),
+                              CheckIfProductNameExists(product.ProductName),
+                              CheckIfCategoryFull()
+                              );
+
+            if (result != null)
+            {
+                return result;
+            }
 
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);
+
+            
+
         }
 
         public IDataResult<List<Product>> GetAll()
@@ -68,6 +81,49 @@ namespace Business.Concrete
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
+        }
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Update(Product product)
+        {
+            if (CheckIfProductCountOfCategoryCorrect(product.CategoryId).Success)
+            {
+                _productDal.Update(product);
+
+                return new SuccessResult(Messages.ProductAdded);
+            }
+
+            return new ErrorResult();
+        }
+
+        private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
+        {
+            var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
+            if (result >= 10)
+            {
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfProductNameExists(string productName)
+        {
+            var result = _productDal.GetAll(p => p.ProductName == productName).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.SameProductNameError);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfCategoryFull()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Data.Count >= 15)
+            {
+                return new ErrorResult(Messages.FullCategoryError);
+            }
+            return new SuccessResult();
         }
     }
 }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz.";
         public static string MaintenanceTime = "Şu anda bakımdayız.";
         public static string ProductListed = "Ürünler Listelendi.";
+        public static string ProductCountOfCategoryError ="Aynı kategoride maksimum 10 ürün olabilir.";
+        public static string SameProductNameError = "Aynı isimde başka ürün bulunuyor.";
+        public static string FullCategoryError = "Kategori sayısı maksimum 15 olabilir.";
     }
 }
