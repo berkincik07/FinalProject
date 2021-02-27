d2488eebe2d80aa49b19cdee556db8631b6e82c9
Root <Root@DESKTOP-89ILC7V>
2021-02-28 01:15:05 +0300

    Ders Sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -13,5 +15,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError ="Aynı kategoride maksimum 10 ürün olabilir.";
         public static string SameProductNameError = "Aynı isimde başka ürün bulunuyor.";
         public static string FullCategoryError = "Kategori sayısı maksimum 15 olabilir.";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserRegistered = "Kullanıcı kaydedildi.";
+        public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string PasswordError = "Şifre hatalı.";
+        public static string SuccessfulLogin = "Giriş başarılı.";
+        public static string UserAlreadyExists = "Kullanıcı zaten kayıtlı.";
+        public static string AccessTokenCreated = "Erişim tokeni oluşturuldu.";
     }
 }
